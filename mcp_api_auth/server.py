72186caa1c37a10bc62e37504cf3dc44821bb00e
sanjay23 <sanjay23@cisco.com>
2025-10-07 20:45:04 +0530

    fix

--- mcp_api_auth/server.py
+++ mcp_api_auth/server.py
@@ -155,11 +155,12 @@ async def generate_uuid(count: int = 1) -> str:
 def main():
     parser = argparse.ArgumentParser(description="Run MCP Streamable HTTP based server")
     parser.add_argument("--transport", type=str, default="http", help="transport for the app")
+    parser.add_argument("--host", type=str, default="localhost", help="host to run the app")
     parser.add_argument("--port", type=int, default=8123, help="Localhost port to listen on")
     args = parser.parse_args()
 
     # Start the server with Streamable HTTP transport
-    uvicorn.run(mcp.streamable_http_app, host="localhost", port=args.port)
+    uvicorn.run(mcp.streamable_http_app, host=args.host, port=args.port)
 
 
 if __name__ == "__main__":
